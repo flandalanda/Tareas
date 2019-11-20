597e0e0d715ea9aa6d31b1e7a2a5f6934cc9b146
sdist <sdist@inge.itam.mx>
2019-11-20 14:26:45 -0600

    ej4

--- TareaCB/ejer4.asm
+++ TareaCB/ejer4.asm
@@ -9,8 +9,15 @@ sdwrd SDWORD ?
 texto1 BYTE "Introduzca un entero sin signo: ",0
 texto2 BYTE "Introduzca un entero signado: ",0
 
-; dwToStr
+strDword BYTE 10 DUP(?),0
+strSDword BYTE 11 DUP(?),0
 
+; dwToStr
+dirRet DWORD ?
+
+
+; sdwToStr
+dirRet2 DWORD ?
 
 .code
 main PROC
@@ -19,6 +26,12 @@ main PROC
     CALL WriteString
     CALL ReadInt
     MOV dwrd, EAX
+
+    PUSH dwrd
+    PUSH OFFSET strDWord
+    CALL dwToStr
+
+
 
     MOV EDX, OFFSET texto2
     CALL WriteString
@@ -43,6 +56,38 @@ dwToStr PROC
 
     ret
 dwToStr ENDP
+
+;--------------------------------------------------------
+sdwToStr PROC
+; DESCRIPCION:
+;   Convierte SDWORD a cadena de caracteres que representan al número introducido.
+; RECIBE:
+; Por medio del stack,
+;   el valor de la SDWORD y la dirección inicial del buffer donde quedará la cadena;
+; REGRESA:
+;   Nada.
+;--------------------------------------------------------
+    POP dirRet2
+    POP ESI                 ; apuntador buffer string
+    POP EAX                 ; valor del SDWORD
+
+    .IF PTR SDWORD EAX < 0
+        MOV [ESI], "+"
+    .ELSE 
+        MOV [ESI], "-"
+    .ENDIF
+
+    MOV EBX, 1
+
+    .WHILE PTR SDWORD EAX != 0
+
+        CALL WriteHexB
+        SHR EAX, 8
+
+    .ENDW 
+        
+    ret
+sdwToStr ENDP
 
 ;-----------------------------------------------------------
 
